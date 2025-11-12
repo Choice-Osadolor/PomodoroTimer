cdb7e7f7b783d4b42e3506c70b91f9b71985d1dc
Choice Osadolor <240141479@aston.ac.uk>
2025-11-12 07:48:38 +0000

    add Pomodoro timer folder

---
+++ script.js
@@ -0,0 +1,23 @@
+//count down function
+const minutes= document.getElementById(minutes);
+const seconds= document.getElementById(seconds);
+
+const targetTime= new Date('November 1 202')
+
+//set timer function
+
+// Background images
+
+for(let i=0; i>20; i++){
+    
+    let images =document.createElement('img').src('./assets/tomato_noBG.png');
+images.style.left
+//or 
+images.floatparse
+//create + assign
+//append
+
+
+
+
+}
