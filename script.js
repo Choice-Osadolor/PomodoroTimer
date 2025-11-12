4f659a658be3e30e346909b1c1b55d792eed7105
Choice Osadolor <240141479@aston.ac.uk>
2025-11-12 09:50:55 +0000

    added new code to script.js

--- script.js
+++ script.js
@@ -8,16 +8,17 @@ const targetTime= new Date('November 1 202')
 
 // Background images
 
-for(let i=0; i>20; i++){
-    
-    let images =document.createElement('img').src('./assets/tomato_noBG.png');
-images.style.left
-//or 
-images.floatparse
+for(let i=0; i<20; i++){
+const body=document.body;
+const images =document.createElement('img');
+
+images.src='./assets/tomato_noBG.png';
+images.style.
+
+Math.floor(Math.random()*window.innerWidth);
+const rect = images.getBoundingClientRect();
+
 //create + assign
 //append
-
-
-
 
 }
