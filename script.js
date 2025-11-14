683cff46eaff5a0f8275017fccb2af93d648171c
Choice Osadolor <240141479@aston.ac.uk>
2025-11-14 23:56:07 +0000

    Javascript Timer function has been added

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 //count down function
 
+
+
 //set timer function
+const minuteDisplay= document.getElementById('minuteDisplay');
+const secondDisplay = document.getElementbyID('secondDisplay');
+const minutes=25;
+const seconds=0;
+const isPaused=false;
+
+function start(){
+if(!isPaused){
+seconds++
+if(seconds>60){
+  minutes++
+}
+minuteDisplay.textContent=minutes;
+}
+}
+
+function pause(){
+isPaused = true;
+}
+
+function play(){
+  isPaused=false;
+}
+
+setInterval(start, 1000);//every second
+setInterval(start, 60000);//every minute
+
+
 
 // Background images
 for(let i=0; i<20; i++){
